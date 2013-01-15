ebba87c436f074095fc170416a2ec047a4e3848c
Michael J. Cox <mike.jon.cox@gmail.com>
2013-01-15 16:52:46 -0700

    initial commit

---
+++ swim_parser.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+import lex
+from ply import lex
+tokens = (
+	"SYMBOL",
+	"COUNT"
+)
+t_SYMBOL = (
+	r"{|}|x|X"
+)
+def t_COUNT(t):
+    r"\d+"
+    t.value=int(t.value)
+    return t
+def t_error(t):
+    raise TypeError("Unknown text '%s'" % (t.value,))
+lex.lex()
+wkout = "{2x50}"
+lex.input(wkout)
+for tok in iter(lex.token, None):
+    print repr(tok.type), repr(tok.value)
+    
