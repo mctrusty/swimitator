d83092fada2729bb6b11dce6da90a5ca747cb723
Michael J. Cox <mike.jon.cox@gmail.com>
2013-01-18 20:39:53 -0700

    Added generic Node class to parser to start structuring the output. I will probably institute more concrete classes once I flesh out the grammar a bit better. I wanted to use the class-based structure suggested in section 6.10 (AST Construction) of the PLY manual because I want to have a flexible way to generate code from the parser.

--- swim_parser.py
+++ swim_parser.py
@@ -3,31 +3,53 @@
 # it uses David Beazley's PLY parser to do the parsing 
 # @author: Michael J. Cox
 
-import pdb
 from ply import lex
 from ply import yacc
 from swim_lexer import tokens
 
-def p_stroke(p):
+class Node:
+	def __init__(self, type, children=None, leaf=None):
+		self.type = type
+		if children:
+			self.children = children
+		else:
+			self.children = []
+		self.leaf = leaf
+		
+	def __repr__(self):
+		return "Set info %s" % (self.children)
+
+def p_set(p):
 	"""
 	set : 
-	set : count
+	set : count skill
 	"""
 	if len(p) == 1:
 		p[0] = []
 	else:
-		p[0] = p[1] 
+		p[0] = Node("set",[p[1] ,p[2]])
 	
 def p_count(p):
 	"""
 	count : NUMBER
 			 | NUMBER MULT NUMBER
 	"""
-	pdb.set_trace()
 	if len(p) == 2:
 		p[0] = (1,p[1])
 	elif len(p) == 4:
 		p[0] = (p[1] ,p[3])
+
+def p_skill(p):
+	"""
+	skill : DRILL
+	"""
+	if len(p) == 3:
+		p[0] = (p[1],[p2])
+	else:
+		p[0] = (p[1],'')
+
+def p_error(p):
+	raise TypeError("Unknown Text '%s'" % (t.value,))
 
 import logging
 logging.basicConfig(
