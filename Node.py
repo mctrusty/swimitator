e8183e2126c75d536b7234e3938edfa90c6cca3b
Michael J. Cox <mike.jon.cox@gmail.com>
2013-01-21 17:30:53 -0700

    Node.py contains the start of an AST structure for encapsulating workout information. Right now, it just consists of one general purpose "Node" class

---
+++ Node.py
@@ -0,0 +1,11 @@
+class Node:
+	def __init__(self, type, children=None, leaf=None):
+		self.type = type
+		if children:
+			self.children = children
+		else:
+			self.children = []
+		self.leaf = leaf
+		
+	def __repr__(self):
+		return "Set info %s" % (self.children)
