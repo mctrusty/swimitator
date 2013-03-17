bb7daacea3e5079a057cc363d90975f76a54c001
Michael J. Cox <mike.jon.cox@gmail.com>
2013-03-17 07:56:02 -0600

    changed swim_xml.py to parse.py because it now has a method to output JSON.

--- swimitator/parse.py
+++ swimitator/parse.py
@@ -1,6 +1,7 @@
 import swim_parser
 import swim_ast.traversal as traversal
 from swim_ast.xml_visitor import SwimXmlVisitor, SwimXmlCloseVisitor
+from swim_ast.json_visitor import SwimJsonVisitor, SwimJsonCloseVisitor
 
 def get_xml(workout):
     p = swim_parser.parser
@@ -10,4 +11,13 @@ def get_xml(workout):
     out = []
     traversal.xml_traverse(res, sv, cv, out)
     out.insert(0, '<?xml version="1.0" encoding="utf-8"?>')
+    return '\n'.join(out)
+
+def get_json(workout):
+    p = swim_parser.parser
+    res = p.parse(workout)
+    sv = SwimJsonVisitor()
+    cv = SwimJsonCloseVisitor()
+    out = []
+    traversal.json_traverse(res, sv, cv, out)
     return '\n'.join(out)
